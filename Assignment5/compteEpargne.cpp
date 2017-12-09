b0cd2c0d8a68abb90202f209f26b893dd202108a
Nicholas Broadbent <nbroa025@uottawa.ca>
2017-12-08 22:01:03 -0500

    Update compteEpargne.cpp

--- Assignment5/compteEpargne.cpp
+++ Assignment5/compteEpargne.cpp
@@ -10,21 +10,21 @@ using namespace std;
 
 CompteEpargne::CompteEpargne(double amount, double interest) : Compte(amount), interet(interest)
 {
-    if (solde < 0.0)
-    {
-        cout << "Error: Balance cannot be less than 0. Setting to 0" << endl << endl;
-        solde = 0.0;
-    }
-    if (interet < 0.0)
-    {
-        cout << "Error: Interest rate cannot be less than 0. Setting to 0" << endl << endl;
-        interet = 0.0;
-    }
+	if (solde < 0.0)
+	{
+		cout << "Error: Balance cannot be less than 0. Setting to 0" << endl << endl;
+		solde = 0.0;
+	}
+	if (interet < 0.0)
+	{
+		cout << "Error: Interest rate cannot be less than 0. Setting to 0" << endl << endl;
+		interet = 0.0;
+	}
 }
 
 double CompteEpargne::calculateInterest()
 {
-    return solde * (interet / 100);
+	return solde * (interet / 100);
 }
 
 double CompteEpargne::getInterest()
@@ -34,5 +34,5 @@ double CompteEpargne::getInterest()
 
 string CompteEpargne::getAccountType()
 {
-    return "savings";
+	return "savings";
 }
