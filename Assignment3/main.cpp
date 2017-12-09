e81b540f437d7a22f123349286876cbed0950b76
Nicholas Broadbent <nbroa025@uottawa.ca>
2017-12-08 22:08:54 -0500

    Update main.cpp

--- Assignment3/main.cpp
+++ Assignment3/main.cpp
@@ -15,7 +15,6 @@ void wordStats(string word);
 
 int main()
 {
-
 	string s;
 	string words = "";
 
@@ -33,64 +32,62 @@ int main()
 		for (int i = 0; i < s.length(); i++)
 		{
 			// Skip spaces before first word.
-            if (!charFound && s[i] == ' ')
+           		 if (!charFound && s[i] == ' ')
 				continue;
 
 			// Reduce multiple spaces to one.
-            if (i < s.length()-1 && s[i] == ' ' && s[i + 1] == ' ')
+            		if (i < s.length()-1 && s[i] == ' ' && s[i + 1] == ' ')
 				continue;
 
 			parsed += s[i];
-            charFound = true;
+            		charFound = true;
 		}
 
 		// Ensure only 1 space after word.
-        if (parsed[parsed.length() - 1] != ' ')
+		if (parsed[parsed.length() - 1] != ' ')
 			parsed += " ";
 
-        //Stop reading when word "stop" is found.
-        if (parsed == "stop ") // 'stop' was entered by itself on its own line.
-        {
-            break;
-        }
-        else if (findWord(s, "stop")) // 'stop' was entered mid-sentence.
-        {
+		//Stop reading when word "stop" is found.
+		if (parsed == "stop ") // 'stop' was entered by itself on its own line.
+		{
+		    break;
+		}
+		else if (findWord(s, "stop")) // 'stop' was entered mid-sentence.
+		{
 
-            parsed = parsed.substr(0, parsed.find("stop"));
-            words += parsed; // Add substring before 'stop' before breaking the loop.
-            break;
-        }
+		    parsed = parsed.substr(0, parsed.find("stop"));
+		    words += parsed; // Add substring before 'stop' before breaking the loop.
+		    break;
+		}
 
 		words += parsed;
-
 	}
 
 	// Remove space at end of sentence.
-    if (words[words.length() - 1] == ' ')
+    	if (words[words.length() - 1] == ' ')
 		words.erase(words.end()-1);
 
-    // Convert std::string to C-style string (array of char).
-    char cStringWords[words.length() + 1];
-    strcpy(cStringWords, words.c_str());
+	// Convert std::string to C-style string (array of char).
+	char cStringWords[words.length() + 1];
+	strcpy(cStringWords, words.c_str());
 
-    // Points to and iterates through array of char C-style string.
-    char* pntCStringWords = &cStringWords[0];
+	// Points to and iterates through array of char C-style string.
+	char* pntCStringWords = &cStringWords[0];
 
-    cout << endl;
+	cout << endl;
 
-    // Prints information for each word in C-style string.
-    while (*pntCStringWords != NULL)
-    {
-       double mean = oldstring::mean(pntCStringWords);
-       double deviation = oldstring::stdDev(pntCStringWords, mean);
-       string word = oldstring::getWord(&pntCStringWords);
+	// Prints information for each word in C-style string.
+	while (*pntCStringWords != NULL)
+	{
+		double mean = oldstring::mean(pntCStringWords);
+		double deviation = oldstring::stdDev(pntCStringWords, mean);
+		string word = oldstring::getWord(&pntCStringWords);
 
-       cout << "Mean of " << word << ": " << mean << endl;
-       cout << "Standard deviation: " << deviation << endl;
-    }
+		cout << "Mean of " << word << ": " << mean << endl;
+		cout << "Standard deviation: " << deviation << endl;
+	}
 
 	return 0;
-
 }
 
 /*
@@ -100,21 +97,17 @@ int main()
  */
 bool findWord(string text, string find)
 {
-
 	string word = "";
 
-    for (const char c : text)
+    	for (const char c : text)
 	{
-
-        word += c;
+        	word += c;
 
 		if (word == find)
-            return true;
+            		return true;
 
-        if (c == ' ')
-            word = "";
-
-    }
-
+        	if (c == ' ')
+           		word = "";
+    	}
 	return false;
 }
