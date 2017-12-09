e5eae6244b7ea7f2055189e60af1f6aab143b99a
Nicholas Broadbent <nbroa025@uottawa.ca>
2017-12-08 22:02:30 -0500

    Update compteCheque.cpp

--- Assignment5/compteCheque.cpp
+++ Assignment5/compteCheque.cpp
@@ -10,16 +10,16 @@ using namespace std;
 
 CompteCheque::CompteCheque(double amount, double fee) : Compte(amount), frais(fee)
 {
-    if (solde < 0.0)
-    {
-        cout << "Error: Balance cannot be less than 0. Setting to 0" << endl << endl;
-        solde = 0.0;
-    }
-    if (frais < 0.0)
-    {
-        cout << "Error: Transaction fee cannot be less than 0. Setting to 0" << endl << endl;
-        frais = 0.0;
-    }
+	if (solde < 0.0)
+	{
+		cout << "Error: Balance cannot be less than 0. Setting to 0" << endl << endl;
+		solde = 0.0;
+	}
+	if (frais < 0.0)
+	{
+		cout << "Error: Transaction fee cannot be less than 0. Setting to 0" << endl << endl;
+		frais = 0.0;
+	}
 }
 
 CompteCheque::CompteCheque(CompteCheque &cheque)
@@ -30,34 +30,33 @@ CompteCheque::CompteCheque(CompteCheque &cheque)
 
 void CompteCheque::credit(double num)
 {
-    solde += num;
-    solde -= frais;
+	solde += num;
+	solde -= frais;
 
-    if (solde < 0.0)
-    {
-        cout << "Error: Not enough money to pay the transaction fee" << endl;
-        solde -= num;
-        solde += frais;
-
-    }
+	if (solde < 0.0)
+	{
+		cout << "Error: Not enough money to pay the transaction fee" << endl;
+		solde -= num;
+		solde += frais;
+	}
 }
 
 bool CompteCheque::debit(double num)
 {
-    if (Compte::debit(num))
-    {
-        solde -= frais;
+	if (Compte::debit(num))
+	{
+		solde -= frais;
 
-        if (solde < 0.0)
-        {
-            cout << "Error: Not enough money to pay the transaction fee" << endl;
-            solde += num;
-            solde += frais;
+		if (solde < 0.0)
+		{
+			cout << "Error: Not enough money to pay the transaction fee" << endl;
+			solde += num;
+			solde += frais;
 			return false;
-        }
+		}
 		return true;
-    }
-    return false;
+	}
+	return false;
 }
 
 double CompteCheque::getFees()
@@ -67,5 +66,5 @@ double CompteCheque::getFees()
 
 string CompteCheque::getAccountType()
 {
-    return "checking";
+	return "checking";
 }
