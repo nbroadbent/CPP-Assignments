b7b6d71223bc9523cf9062245ad11d8bfa488127
Nicholas Broadbent <nbroa025@uottawa.ca>
2017-12-08 22:06:03 -0500

    Update compteCheque.cpp

--- Assignment4/compteCheque.cpp
+++ Assignment4/compteCheque.cpp
@@ -11,48 +11,47 @@ using namespace std;
 
 CompteCheque::CompteCheque(double amount, double fee) : Compte(amount), frais(fee)
 {
-    if (solde < 0.0)
-    {
-        cout << "Erreur: Solde ne peut pas être moins de zero" << endl;
-        solde = 0.0;
-    }
-    if (frais < 0.0)
-    {
-        cout << "Erreur: Frais ne peut pas être moins de zero" << endl;
-        frais = 0.0;
-    }
+	if (solde < 0.0)
+	{
+		cout << "Erreur: Solde ne peut pas être moins de zero" << endl;
+		solde = 0.0;
+	}
+	if (frais < 0.0)
+	{
+		cout << "Erreur: Frais ne peut pas être moins de zero" << endl;
+		frais = 0.0;
+	}
 }
 
 void CompteCheque::credit(double num)
 {
-    solde += num;
-    solde -= frais;
+	solde += num;
+	solde -= frais;
 
-    if (solde < 0.0)
-    {
-        cout << "Erreur: Pas assez d'argent pour payer le frais" << endl;
-        solde -= num;
-        solde += frais;
-
-    }
+	if (solde < 0.0)
+	{
+		cout << "Erreur: Pas assez d'argent pour payer le frais" << endl;
+		solde -= num;
+		solde += frais;
+	}
 }
 
 bool CompteCheque::debit(double num)
 {
-    if (Compte::debit(num))
-    {
-        solde -= frais;
+	if (Compte::debit(num))
+	{
+		solde -= frais;
 
-        if (solde < 0.0)
-        {
-            cout << "Erreur: Pas assez d'argent pour payer le frais" << endl;
-            solde += num;
-            solde += frais;
+		if (solde < 0.0)
+		{
+			cout << "Erreur: Pas assez d'argent pour payer le frais" << endl;
+			solde += num;
+			solde += frais;
 			return false;
-        }
+		}
 		return true;
-    }
-    return false;
+	}
+	return false;
 }
 
 double CompteCheque::getFees()
